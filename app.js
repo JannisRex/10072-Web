73e915609efe15083014ebcc4c82ff36722ff818
JannisRex <rex.jannis@web.de>
2022-01-10 16:35:07 +0100

    [adjusted] initial structure
    Signed-off-by: JannisRex <rex.jannis@web.de>

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
+// Importing Modules
+require('dotenv').config()
 let createError = require('http-errors')
 let express = require('express')
 let path = require('path')
 let cookieParser = require('cookie-parser')
 let logger = require('morgan')
 
+// Router contains ALL routes for specific group of routes
+// e.g. userRouter routes all pages affiliated with user-specific tasks
 let indexRouter = require('./routes/index')
 let usersRouter = require('./routes/users')
 
 let app = express()
 
-// view engine setup
+// HBS gets used as view engine
+// so we initialize and set it
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 
+// setting webpack-devServer (just for live-reload)
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+// set location for html (and assets) to serve
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Routes
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 
