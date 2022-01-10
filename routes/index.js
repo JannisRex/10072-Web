f3b734a1244da0c5fdafe9b545b449f3bf318635
JannisRex <rex.jannis@web.de>
2022-01-10 18:02:21 +0100

    [add] new testRoute
    Signed-off-by: JannisRex <rex.jannis@web.de>

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'flüssiges.de' })
 })
 
+router.get('/test', function (req, res, next) {
+  res.render('test', { title: 'flüssiges.de' })
+})
+
 module.exports = router
